315fd5a5dacb0645b85d9f64f5e2f6f8222eb869
leodoc0407 <leodoc0407@gmail.com>
2026-06-05 00:49:28 +0300

    Update AdminToolsMod.java

---
+++ src/main/java/staff/helepr/AdminToolsMod.java
@@ -0,0 +1,30 @@
+package staff.helepr;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import staff.helepr.screen.MainScreen;
+
+public class AdminToolsMod implements ClientModInitializer {
+    private static KeyBinding openGuiKey;
+
+    @Override
+    public void onInitializeClient() {
+        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.staffhelper.open_gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_0,
+                "category.staffhelper"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openGuiKey.wasPressed()) {
+                client.setScreen(new MainScreen());
+            }
+        });
+    }
+}
